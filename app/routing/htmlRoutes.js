a0948b46d1a5defd145860198aac271c4fe11513
John Pendergrass <john.pendergrass@me.com>
2020-04-30 12:44:34 -0500

    setup pathways for routes and to HTML pages

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,20 @@
+// ===============================================================================
+// LOAD DATA
+// ===============================================================================
+var path = require("path");
+
+module.exports = function(app) {
+    
+    app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    app.get("/survey", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    
+    // fallback use route for homepage
+	app.use(function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+};
