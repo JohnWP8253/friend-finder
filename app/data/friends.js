f1104518f7eac35ab9dd44493394591698053c90
John Pendergrass <john.pendergrass@me.com>
2020-05-04 12:50:39 -0500

    added modal, questions, friends, and js at the end of the survey page

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,44 +2,44 @@ var friends = [
   {
     name: "Frodo",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+      "https://vignette.wikia.nocookie.net/lotr/images/5/54/Untitledjk.png/revision/latest?cb=20130313174543",
+    scores: [4, 1, 5, 4, 4, 3, 4, 2, 4, 1],
   },
   {
     name: "Sam",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+      "https://i0.wp.com/www.theonering.net/torwp/wp-content/uploads/2013/05/samwise-gamgee.jpg?fit=444%2C273&ssl=1",
+    scores: [2, 1, 5, 1, 3, 3, 4, 2, 3, 1],
   },
   {
-    name: "Merry",
+    name: "Gollum",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+    scores: [5, 5, 1, 4, 5, 1, 1, 5, 1, 1],
   },
   {
     name: "Gandalf",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+      "https://cnet3.cbsistatic.com/img/DHoXqP1XGX8GhumaBQIbnaGxlBw=/940x0/2014/12/01/89601d70-ca65-4dc7-adfe-f6dbd46d05ba/hobbit-lotr-5-armies-gandalf-hat.jpg",
+    scores: [1, 1, 5, 1, 3, 5, 2, 3, 4, 1],
   },
   {
     name: "Gimli",
     photo:
-      "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+      "https://vignette.wikia.nocookie.net/lotr/images/4/43/Gimli.jpg/revision/latest?cb=20080318220808",
+    scores: [4, 3, 3, 2, 1, 1, 3, 3, 2, 2],
   },
   {
-    name: "Ahmed",
+    name: "Saruman",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+    scores: [4, 3, 3, 4, 2, 5, 2, 1, 4, 4],
   },
   {
-    name: "Ahmed",
+    name: "Wormtail",
     photo:
       "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+    scores: [4, 2, 3, 1, 3, 1, 1, 3, 2, 3],
   },
 
 ];
